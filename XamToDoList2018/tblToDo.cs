83b74eb3bf2b7dc3cecd933d1f6a189c74770938
himanshu dahiya <ashuad5000@gmail.com>
2019-10-22 11:26:40 +1300

    Add project files.

---
+++ XamToDoList2018/tblToDo.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace XamToDoList2018
+{
+    public class tblToDo
+    {
+        [PrimaryKey, AutoIncrement]  //These are attributes that define the property below it
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime Date { get; set; }
+
+        public tblToDo()
+        {
+        }
+
+    }
+}
